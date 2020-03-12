54571c0884b4ef30524602e91c7bf4e90f0dcbce
Aurélien Christman <christman.aurelien@bcgplatinion.com>
2020-03-12 16:05:56 +0100

    service -> space

--- vars/cfDeploy.groovy
+++ vars/cfDeploy.groovy
@@ -11,7 +11,7 @@ def call(Map params) {
     } else {
         echo "Deploying ..."
         withCredentials([usernamePassword(credentialsId: 'pcfdev_user', usernameVariable: 'username', passwordVariable: 'password')]) {
-            sh "CF_HOME=\$(pwd) cf login -a api.run.pivotal.io -u \"${username}\" -p \"${password}\" -o aurelien -s \"${params.service}\""
+            sh "CF_HOME=\$(pwd) cf login -a api.run.pivotal.io -u \"${username}\" -p \"${password}\" -o aurelien -s \"${params.space}\""
             sh "CF_HOME=\$(pwd) cf push thdevops-\${GIT_BRANCH} -p \"${params.target}\""
         }
     }
