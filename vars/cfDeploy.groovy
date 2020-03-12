1a1521dbf952b3b8399b8aed605e8870c2dd1d85
Aurélien Christman <christman.aurelien@bcgplatinion.com>
2020-03-12 15:48:47 +0100

    Deploying master on staging space

--- vars/cfDeploy.groovy
+++ vars/cfDeploy.groovy
@@ -1,12 +1,14 @@
 def call(Map params) {
-    // capture scm variables
     def scmVars = checkout scm
     String branch = scmVars.GIT_BRANCH
 
-    // or use the environment variable
-    branch = env.GIT_BRANCH
-    
     if (branch == "master") {
+        echo "Deploying ..."
+        withCredentials([usernamePassword(credentialsId: 'pcfdev_user', usernameVariable: 'username', passwordVariable: 'password')]) {
+            sh "CF_HOME=\$(pwd) cf login -a api.run.pivotal.io -u \"${username}\" -p \"${password}\" -o aurelien -s \"Common Staging Space\""
+            sh "CF_HOME=\$(pwd) cf push thdevops-\${GIT_BRANCH} -p \"${params.target}\""
+        }
+    } else {
         echo "Deploying ..."
         withCredentials([usernamePassword(credentialsId: 'pcfdev_user', usernameVariable: 'username', passwordVariable: 'password')]) {
             sh "CF_HOME=\$(pwd) cf login -a api.run.pivotal.io -u \"${username}\" -p \"${password}\" -o aurelien -s \"${params.service}\""
