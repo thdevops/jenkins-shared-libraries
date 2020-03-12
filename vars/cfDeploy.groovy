ff244e9de755cedcb9bb1680d9e6ff87c9a86970
Aurélien Christman <christman.aurelien@bcgplatinion.com>
2020-03-12 11:02:35 +0100

    Adding CF deployment

---
+++ vars/cfDeploy.groovy
@@ -0,0 +1,9 @@
+def call(Map config) {
+    echo "Deploying ..."
+    withCredentials([usernamePassword(credentialsId: 'pcfdev_user', usernameVariable: 'username', passwordVariable: 'password')]) {
+        sh '''
+            CF_HOME=$(pwd) cf login -a api.run.pivotal.io -u \"${username}\" -p \"${password}\" -o aurelien -s ${config.service}
+            CF_HOME=$(pwd) cf push thdevops-test -p ./target/springboot-appengine-standard-0.0.1-SNAPSHOT.war
+        '''
+    }
+}
