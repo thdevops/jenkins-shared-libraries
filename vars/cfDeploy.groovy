a351dd9f24ad1ffc5a515e1f726a516f5826bb10
Aurélien Christman <christman.aurelien@bcgplatinion.com>
2020-03-12 11:14:16 +0100

    Fixing cfDeploy

--- vars/cfDeploy.groovy
+++ vars/cfDeploy.groovy
@@ -1,9 +1,7 @@
 def call(Map config) {
     echo "Deploying ..."
     withCredentials([usernamePassword(credentialsId: 'pcfdev_user', usernameVariable: 'username', passwordVariable: 'password')]) {
-        sh '''
-            CF_HOME=$(pwd) cf login -a api.run.pivotal.io -u \"${username}\" -p \"${password}\" -o aurelien -s ${config.service}
-            CF_HOME=$(pwd) cf push thdevops-test -p ${config.target}
-        '''
+        sh "CF_HOME=$(pwd) cf login -a api.run.pivotal.io -u \"${username}\" -p \"${password}\" -o aurelien -s ${config.service}"
+        sh  "CF_HOME=$(pwd) cf push thdevops-test -p ${config.target}"
     }
 }
