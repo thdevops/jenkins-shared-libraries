1a786f4252a8607cfd7d1cf7af4a437df62e68ce
Aurélien Christman <christman.aurelien@bcgplatinion.com>
2020-03-13 14:01:10 +0100

    Parsing pom.xml

--- vars/cfDeploy.groovy
+++ vars/cfDeploy.groovy
@@ -7,10 +7,10 @@ def call(Map params) {
     def path = pwd()
 
     def pom = new XmlSlurper().parse(path + "/pom.xml")
-    //String artifactId = xmlText.project.artifactId
 
-    echo pom.name()
-    echo pom.artifactId.text()
+    String artifactId = pom.artifactId.text()
+    String version = pom.version.text()
+    String packaging = pom.packaging.text()
 
     if (branch == "master") {
         space = "Common Staging Space"
@@ -19,6 +19,6 @@ def call(Map params) {
     echo "Deploying ..."
     withCredentials([usernamePassword(credentialsId: 'pcfdev_user', usernameVariable: 'username', passwordVariable: 'password')]) {
         sh "CF_HOME=\$(pwd) cf login -a api.run.pivotal.io -u \"${username}\" -p \"${password}\" -o aurelien -s \"${space}\""
-        sh "CF_HOME=\$(pwd) cf push thdevops-\${GIT_BRANCH} -p \"${params.target}\""
+        sh "CF_HOME=\$(pwd) cf push thdevops-\${GIT_BRANCH} -p \"${artifactId}-${version}.${packaging}\""
     }
 }
