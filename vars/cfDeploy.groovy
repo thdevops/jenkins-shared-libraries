5ea5dfc2e1bce523f96bbe16b2f258bf2a1c7306
Aurélien Christman <christman.aurelien@bcgplatinion.com>
2020-03-13 08:53:28 +0100

    Refactoring

--- vars/cfDeploy.groovy
+++ vars/cfDeploy.groovy
@@ -2,17 +2,15 @@ def call(Map params) {
     def scmVars = checkout scm
     String branch = scmVars.GIT_BRANCH
 
+    String space = params.space
+
     if (branch == "master") {
-        echo "Deploying ..."
-        withCredentials([usernamePassword(credentialsId: 'pcfdev_user', usernameVariable: 'username', passwordVariable: 'password')]) {
-            sh "CF_HOME=\$(pwd) cf login -a api.run.pivotal.io -u \"${username}\" -p \"${password}\" -o aurelien -s \"Common Staging Space\""
-            sh "CF_HOME=\$(pwd) cf push thdevops-\${GIT_BRANCH} -p \"${params.target}\""
-        }
-    } else {
-        echo "Deploying ..."
-        withCredentials([usernamePassword(credentialsId: 'pcfdev_user', usernameVariable: 'username', passwordVariable: 'password')]) {
-            sh "CF_HOME=\$(pwd) cf login -a api.run.pivotal.io -u \"${username}\" -p \"${password}\" -o aurelien -s \"${params.space}\""
-            sh "CF_HOME=\$(pwd) cf push thdevops-\${GIT_BRANCH} -p \"${params.target}\""
-        }
+        space = "Common Staging Space"
+    }
+
+    echo "Deploying ..."
+    withCredentials([usernamePassword(credentialsId: 'pcfdev_user', usernameVariable: 'username', passwordVariable: 'password')]) {
+        sh "CF_HOME=\$(pwd) cf login -a api.run.pivotal.io -u \"${username}\" -p \"${password}\" -o aurelien -s \"${space}\""
+        sh "CF_HOME=\$(pwd) cf push thdevops-\${GIT_BRANCH} -p \"${params.target}\""
     }
 }
