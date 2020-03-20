e3425985e7670c490cccd7430827fb75a66b2796
Aurélien Christman <christman.aurelien@bcgplatinion.com>
2020-03-20 09:50:30 +0100

    Use Maven image, install CF cli on deploy

--- vars/cfDeploy.groovy
+++ vars/cfDeploy.groovy
@@ -18,7 +18,12 @@ def call(Map params) {
     }
 
     // Install CloudFoundry CLI
-
+    sh '''
+wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | apt-key add -
+echo "deb https://packages.cloudfoundry.org/debian stable main" | tee /etc/apt/sources.list.d/cloudfoundry-cli.list
+apt-get update
+apt-get install cf-cli
+    '''
 
     echo "Deploying to ${space} ..."
     withCredentials([usernamePassword(credentialsId: 'pcfdev_user', usernameVariable: 'username', passwordVariable: 'password')]) {
--- vars/mvnBuild.groovy
+++ vars/mvnBuild.groovy
@@ -1,10 +1,4 @@
 def call(Map params) {
-    sh '''
-wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | apt-key add -
-echo "deb https://packages.cloudfoundry.org/debian stable main" | tee /etc/apt/sources.list.d/cloudfoundry-cli.list
-apt-get update
-apt-get install cf-cli
-    '''
 
     sh 'mvn package'
 
